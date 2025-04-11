526bc50ea79f3ecd38f8d742e5559fe434fe986b
Samu <reydeunojo2@gmail.com>
2025-04-11 14:26:40 +0200

    actualizacion

--- ProyectoFinal/src/main/java/controlador/Main.java
+++ ProyectoFinal/src/main/java/controlador/Main.java
@@ -1,7 +1,29 @@
 package controlador;
 
-public class Main {
-    public static void main(String[] args) {
+import utils.ConnectionManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/vista/vista.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 350);
+        /*Cargar css en la escena */
+        scene.getStylesheets().add(getClass().getResource("/estilos.css").toExternalForm());
+        stage.setTitle("Dogpuccino");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        /*Conectar con la base de datos*/
+        Connection con = ConnectionManager.getInstance().getConnection();
+        launch();
     }
 }
