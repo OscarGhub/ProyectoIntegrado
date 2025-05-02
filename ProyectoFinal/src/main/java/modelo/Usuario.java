70778ca406abafa73d523bcb83c531ada5117f62
Samu <reydeunojo2@gmail.com>
2025-05-02 13:44:02 +0200

    guardado de protectora tabla

--- ProyectoFinal/src/main/java/modelo/Usuario.java
+++ ProyectoFinal/src/main/java/modelo/Usuario.java
@@ -1,4 +1,40 @@
 package modelo;
 
 public class Usuario {
+    private String username;
+    private String password;
+    private String gmail;
+
+    public Usuario(String username, String password, String gmail) {
+        this.username = username;
+        this.password = password;
+        this.gmail = gmail;
+    }
+    public Usuario() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
 }
