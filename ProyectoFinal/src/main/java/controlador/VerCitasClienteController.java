8aa339ce9eae0949ab5a3ab35b86a1e8b6abc8b1
Samu <reydeunojo2@gmail.com>
2025-05-14 13:26:10 +0200

    creacion y modificacion citas y adopciones en la misma pestaña

--- ProyectoFinal/src/main/java/controlador/VerCitasClienteController.java
+++ ProyectoFinal/src/main/java/controlador/VerCitasClienteController.java
@@ -20,95 +20,29 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class VerCitasClienteController implements Initializable {
+public class VerCitasClienteController {
 
-    @FXML private ImageView imgUsuario;
-
-    @FXML private TableView<CitasInfo> tablaCitas;
-    @FXML private TableColumn<CitasInfo, String> colEstado;
-    @FXML private TableColumn<CitasInfo, Double> colDonacion;
-    @FXML private TableColumn<CitasInfo, LocalDate> colFechaCita;
-    @FXML private TableColumn<CitasInfo, String> colHoraCita;
-    @FXML private TableColumn<CitasInfo, String> colNombreCliente;
-    @FXML private TableColumn<CitasInfo, String> colApellido1;
-    @FXML private TableColumn<CitasInfo, String> colApellido2;
-    @FXML private TableColumn<CitasInfo, String> colCorreoUsuario;
-    @FXML private TableColumn<CitasInfo, String> colNombrePerro;
 
     @FXML
-    private Button btnSolicitarAdp;
-    @FXML
-    private Button btnAjustes;
-    @FXML
-    private Button btnVerPerros;
-    @FXML
-    private Button btnSalir;
-
-    private final ObservableList<CitasInfo> listaCitas = FXCollections.observableArrayList();
-
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        modelo.Animaciones.animarAgrandar(imgUsuario);
-
-        // Configuramos las celdas de las columnas de la tabla
-        colNombreCliente.setCellValueFactory(new PropertyValueFactory<>("nombreCliente"));
-        colApellido1.setCellValueFactory(new PropertyValueFactory<>("apellido1"));
-        colApellido2.setCellValueFactory(new PropertyValueFactory<>("apellido2"));
-        colNombrePerro.setCellValueFactory(new PropertyValueFactory<>("nombrePerro"));
-        colFechaCita.setCellValueFactory(new PropertyValueFactory<>("fechaCita"));
-        colHoraCita.setCellValueFactory(new PropertyValueFactory<>("horaCita"));
-        colCorreoUsuario.setCellValueFactory(new PropertyValueFactory<>("correoUsuario"));
-        colEstado.setCellValueFactory(new PropertyValueFactory<>("estado"));
-        colDonacion.setCellValueFactory(new PropertyValueFactory<>("donacion"));
-
-
-        cargarDatos();
-    }
-
-    @FXML
-    private void cargarDatos() {
-        listaCitas.clear();
-
-        String url = "jdbc:oracle:thin:@localhost:1521:xe";
-        String user = "C##PROYECTOINTEGRADO";
-        String password = "123456";
-
-        String query = "SELECT c.donacion, c.estado, c.fecha_cita, c.hora_cita, " +
-                "cli.nombre, cli.apellido1, cli.apellido2," +
-                "u.correo_electronico AS correo_usuario, p.nombre AS nombre_perro " +
-                "FROM cita c " +
-                "JOIN cliente cli ON c.cliente_id = cli.cliente_id " +
-                "LEFT JOIN usuario_cliente u ON cli.cliente_id = u.cliente_id " +
-                "LEFT JOIN solicitud_adopcion sa ON sa.cliente_id = cli.cliente_id " +
-                "LEFT JOIN perro p ON c.perro_id = p.perro_id";
-
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             PreparedStatement pstmt = conn.prepareStatement(query);
-             ResultSet rs = pstmt.executeQuery()) {
-
-            while (rs.next()) {
-                CitasInfo citas = new CitasInfo(
-                        rs.getDouble("donacion"),
-                        rs.getString("estado"),
-                        rs.getDate("fecha_cita").toLocalDate(),
-                        rs.getString("hora_cita"),
-                        rs.getString("nombre"),
-                        rs.getString("apellido1"),
-                        rs.getString("apellido2"),
-                        rs.getString("correo_usuario"),
-                        rs.getString("nombre_perro")
-                );
-                listaCitas.add(citas);
-            }
-            tablaCitas.setItems(listaCitas);
-
-        } catch (SQLException e) {
-            Logger.getLogger(VerCitasClienteController.class.getName()).log(Level.SEVERE, "Error al cargar los datos de la base de datos", e);
+    void btnAdopcion(ActionEvent event) {
+        try {
+            Ventanas.cerrarVentana(event);
+            Ventanas.abrirVentana("/vista/historialAdopciones.fxml", "Historial Adopciones");
+        } catch (Exception e) {
+            Logger.getLogger(VerCitasClienteController.class.getName()).log(Level.SEVERE, "Error al abrir la ventana de inicio", e);
         }
-
     }
 
-    // Métodos de acción para los botones
+    @FXML
+    void btnCitas(ActionEvent event) {
+        try {
+            Ventanas.cerrarVentana(event);
+            Ventanas.abrirVentana("/vista/historialCitas.fxml", "Historial Citas");
+        } catch (Exception e) {
+            Logger.getLogger(VerCitasClienteController.class.getName()).log(Level.SEVERE, "Error al abrir la ventana de inicio", e);
+        }
+    }
+    // Métodos de acción para los botone
     @FXML
     void btnSalirAc(ActionEvent event) {
         try {
@@ -149,11 +83,5 @@ public class VerCitasClienteController implements Initializable {
         }
     }
 
-    public TableColumn<CitasInfo, Double> getColDonacion() {
-        return colDonacion;
-    }
 
-    public void setColDonacion(TableColumn<CitasInfo, Double> colDonacion) {
-        this.colDonacion = colDonacion;
-    }
 }
